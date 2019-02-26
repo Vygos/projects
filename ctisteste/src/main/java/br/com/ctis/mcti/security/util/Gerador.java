38c632dc736fd2128293cb0d8e399f936d0bfce2
Victor Gabriel <victorsnt123@gmail.com>
2019-02-26 11:45:00 -0300

    [created] custom banner

--- ctisteste/src/main/java/br/com/ctis/mcti/security/util/Gerador.java
+++ ctisteste/src/main/java/br/com/ctis/mcti/security/util/Gerador.java
@@ -1,7 +1,11 @@
 package br.com.ctis.mcti.security.util;
 
+import java.time.*;
+import java.util.Date;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -16,8 +20,11 @@ public class Gerador {
 	public static void main(String... args) {
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 		
-		  System.out.println(bCryptPasswordEncoder.encode("victor"));
-		
+        LocalDate date1 = LocalDate.of(2019,2,1);
+        LocalDate date2 = LocalDate.now();
+        LocalDate date = new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        Period p = Period.between(date1, date2);
+
 	}
 	
 }
